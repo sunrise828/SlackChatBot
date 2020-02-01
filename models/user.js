354590d07c0551edf822156e3907df51f14f2b5b
sunrise <sunrise1115@outlook.com>
2020-02-02 04:13:00 +0800

    changed user model name as 'chartuser'

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
+  const User = sequelize.define('ChatUser', {
     channel: DataTypes.STRING,
     email: DataTypes.STRING,
     name: DataTypes.STRING,
